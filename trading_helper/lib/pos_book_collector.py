316af08614f89db3d33b9d2d47f8ff02cd979c47
cc2709 <c_chuan23@hotmail.com>
2021-05-15 23:37:44 +0100

    Add class - 'PosBookCollector'

---
+++ trading_helper/lib/pos_book_collector.py
@@ -0,0 +1,27 @@
+import requests
+import os
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class PosBookCollInterface:
+    @abstractmethod
+    def get_today_book(self) -> list:
+        pass
+
+
+class PosBookCollector(PosBookCollInterface):
+    def __init__(self, host, version):
+        self.host = host
+        self.version = version
+
+    def get_today_book(self, instrument: str) -> list:
+        url = f"{self.host}/{self.version}/instruments/{instrument}/positionBook"
+        headers = {
+            "Authorization": f"Bearer {os.environ['OANDA_TOKEN']}",
+            "Accept-Datetime-Format": "RFC3339",
+        }
+        r = requests.get(url, headers=headers)
+
+        if r.status_code == 200:
+            pos_book = r.json()["positionBook"]
