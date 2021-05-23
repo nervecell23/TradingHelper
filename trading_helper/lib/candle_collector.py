f9ae55216389d88e1e1b3aac872de86627776f65
cc2709 <c_chuan23@hotmail.com>
2021-05-23 18:08:47 +0100

    Add 'get_daily_candles' method

--- trading_helper/lib/candle_collector.py
+++ trading_helper/lib/candle_collector.py
@@ -9,7 +9,45 @@ class CandleCollector:
         self.host = host
         self.version = version
 
-    def get_today_candle(self, instrument, granularity="D"):
+    def get_daily_candles(
+        self, instrument: str, from_date: datetime, days: int
+    ) -> List[Candle]:
+        """ """
+        from_dt = from_date.replace(hour=0, minute=0, second=0)
+        to_dt = from_dt + timedelta(days=days)
+
+        if to_dt.date() >= datetime.today().date():
+            to_dt = datetime.now()
+
+        url = f"{self.host}/{self.version}/instruments/{instrument}/candles"
+
+        headers = {
+            "Authorization": f"Bearer {os.environ['OANDA_TOKEN']}",
+            "Accept-Datetime-Format": "RFC3339",
+        }
+        params = {
+            "granularity": "D",
+            "from": from_dt.isoformat(),
+            "to": to_dt.isoformat(),
+            "includeFirst": False,
+        }
+        r = requests.get(url, params=params, headers=headers)
+
+        if r.status_code == 200:
+            res_list = []
+            for candle in r.json()["candles"]:
+                candle = Candle(
+                    open=candle["mid"]["o"],
+                    high=candle["mid"]["h"],
+                    low=candle["mid"]["l"],
+                    close=candle["mid"]["c"],
+                    starting_time=candle["time"],
+                )
+                res_list.append(candle)
+            return res_list
+        else:
+            raise Exception(f"ERROR status: {r.status_code}\ndetail: {r.text}")
+
         """ """
         from_time = datetime.now().replace(hour=0, minute=0, second=0)
         to_time = from_time + timedelta(days=1)
