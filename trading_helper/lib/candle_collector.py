7383a02dc849cde47e903a61a66798d1bb8d7106
cc2709 <c_chuan23@hotmail.com>
2021-06-27 20:49:50 +0100

    Refactor 'CandleCollector' class

--- trading_helper/lib/candle_collector.py
+++ trading_helper/lib/candle_collector.py
@@ -9,11 +9,25 @@ class CandleCollector:
     def __init__(self, host, version):
         self.host = host
         self.version = version
+        self.headers = {
+            "Authorization": f"Bearer {os.environ['OANDA_TOKEN']}",
+            "Accept-Datetime-Format": "RFC3339",
+        }
+
+    def _compose_candle(self, candle):
+        return {
+            "Open": float(candle["mid"]["o"]),
+            "High": float(candle["mid"]["h"]),
+            "Low": float(candle["mid"]["l"]),
+            "Close": float(candle["mid"]["c"]),
+            "starting_time": candle["time"],
+            "Date": datetime.strptime(candle["time"], "%Y-%m-%dT%H:%M:%S.%f000Z")
+            + timedelta(days=1),
+        }
 
     def get_daily_candles(
         self, instrument: str, from_date: datetime, days: int
-    ) -> List[Candle]:
-        """ """
+    ) -> List[dict]:
         from_dt = from_date.replace(hour=0, minute=0, second=0)
         to_dt = from_dt + timedelta(days=days)
 
@@ -21,72 +35,37 @@ class CandleCollector:
             to_dt = datetime.now() - timedelta(days=1)
 
         url = f"{self.host}/{self.version}/instruments/{instrument}/candles"
-
-        headers = {
-            "Authorization": f"Bearer {os.environ['OANDA_TOKEN']}",
-            "Accept-Datetime-Format": "RFC3339",
-        }
         params = {
             "granularity": "D",
             "from": from_dt.isoformat(),
             "to": to_dt.isoformat(),
             "includeFirst": False,
         }
-        r = requests.get(url, params=params, headers=headers)
+        r = requests.get(url, params=params, headers=self.headers)
+
+        def map_helper(candle):
+            return self._compose_candle(candle)
 
         if r.status_code == 200:
-            res_list = []
-            for candle in r.json()["candles"]:
-                candle = {
-                    "Open": float(candle["mid"]["o"]),
-                    "High": float(candle["mid"]["h"]),
-                    "Low": float(candle["mid"]["l"]),
-                    "Close": float(candle["mid"]["c"]),
-                    "starting_time": candle["time"],
-                    "Date": datetime.strptime(
-                        candle["time"], "%Y-%m-%dT%H:%M:%S.%f000Z"
-                    )
-                    + timedelta(days=1),
-                }
-                # candle = Candle(
-                #     open=candle["mid"]["o"],
-                #     high=candle["mid"]["h"],
-                #     low=candle["mid"]["l"],
-                #     close=candle["mid"]["c"],
-                #     starting_time=candle["time"],
-                # )
-                res_list.append(candle)
-            return res_list
+            return list(map(map_helper, r.json()["candles"]))
         else:
             raise Exception(f"ERROR status: {r.status_code}\ndetail: {r.text}")
 
-    def get_today_candle(self, instrument: str, granularity: str = "D") -> Candle:
-        """ """
+    def get_today_candle(self, instrument: str, granularity: str = "D") -> dict:
         from_time = datetime.now().replace(hour=0, minute=0, second=0)
-        to_time = from_time + timedelta(days=1)
         url = f"{self.host}/{self.version}/instruments/{instrument}/candles"
-        headers = {
-            "Authorization": f"Bearer {os.environ['OANDA_TOKEN']}",
-            "Accept-Datetime-Format": "RFC3339",
-        }
         params = {
             "granularity": granularity,
             "from": from_time.isoformat(),
-            "to": to_time.isoformat(),
+            "to": (from_time + timedelta(days=1)).isoformat(),
             "includeFirst": False,
         }
-        r = requests.get(url, params=params, headers=headers)
+        r = requests.get(url, params=params, headers=self.headers)
 
         if r.status_code == 200:
             candle = r.json()["candles"][0]
             if candle["complete"]:
-                return Candle(
-                    open=candle["mid"]["o"],
-                    high=candle["mid"]["h"],
-                    low=candle["mid"]["l"],
-                    close=candle["mid"]["c"],
-                    starting_time=candle["time"],
-                )
+                return self._compose_candle(candle)
             else:
                 raise Exception("Did not find complete candle")
         else:
